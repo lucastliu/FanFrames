ca2eb9fc99d2973749445ce5d2735869776fdd5c
Caroline Potts <crp41@duke.edu>
2020-03-26 17:32:53 -0400

    updates messaging protocols, threading

--- client.py
+++ client.py
@@ -2,40 +2,70 @@
 # python client.py --server-ip SERVER_IP
 
 # import the necessary packages
-from imutils.video import VideoStream, WebcamVideoStream
+from imutils.video import VideoStream, WebcamVideoStream, FPS 
 import imagezmq
 import argparse
 import socket
 import time
 import cv2
 
-
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--server-ip", required=True,
 	help="ip address of the server to which the client will connect")
+ap.add_argument("-ms", "--messaging", default=0, type=int,
+	help="type of messaging (default is 0: REQ/REP; 1 is PUB/SUB, 2 is REQ/REP + PUB/SUB)")
+ap.add_argument("-t", "--multithread", default=1, type=int,
+	help="single/multithreading (default is 1: multithreaded; 0 single threaded)")
+
 args = vars(ap.parse_args())
 
 # initialize the ImageSender object with the socket address of the
 # server
-sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
-	args["server_ip"]))
+# initialize the ImageHub object
+if args["messaging"] == 0:
+	sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
+		args["server_ip"]))
+elif args["messaging"] == 1:
+	sender = imagezmq.ImageSender(connect_to="tcp://*:5566", REQ_REP = False)
+elif args["messaging"] == 2:
+	sender = imagezmq.ImageSender(connect_to="tcp://*:5566", REQ_REP = False)
+	rr_sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
+		args["server_ip"]))
+else:
+	raise ValueError("messaging input value must be 0, 1, or 2")
+
 
 # get the host name, initialize the video stream, and allow the
 # camera sensor to warmup
-rpiName = socket.gethostname()
-# vs = VideoStream(usePiCamera=True).start()
-# vs = WebcamVideoStream(src=-1).start()
-vs = VideoStream(src=0).start()
+clientName = socket.gethostname()
+clientIP = socket.gethostbyname(clientName)
 
+if args["multithread"] == 1:
+	vs = WebcamVideoStream(src=0).start()
+elif args["mulithread"] == 0:
+	vs = VideoStream(src=0).start()
+else:
+	raise ValueError("multithread input value must be 0 or 1")
 
-# cap = cv2.VideoCapture(0)
 time.sleep(2.0)
+
+count=0
+fps = FPS().start()
+
  
 while True:
 	# read the frame from the camera and send it to the server
-	# ret, frame = cap.read()
-	# print(frame)
-	# cv2.imshow('frame', frame)
 	frame = vs.read()
-	sender.send_image(rpiName, frame)
+	if count == 0 and args["messaging"] ==2:
+		rr_sender.send_image(clientIP, frame)
+	sender.send_image(clientIP, frame)
+	if count<500:
+		count +=1 
+		fps.update()
+	if count==500:
+		fps.stop()
+		print("[INFO] elasped time to send 500 frames: {:.2f}".format(fps.elapsed()))
+		print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+		count +=1
+	
--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 
 # import the necessary packages
 from imutils import build_montages
+from imutils.video import FPS
 from datetime import datetime
 import numpy as np
 import imagezmq
@@ -22,27 +23,37 @@ ap.add_argument("-mW", "--montageW", required=True, type=int,
 	help="montage frame width")
 ap.add_argument("-mH", "--montageH", required=True, type=int,
 	help="montage frame height")
+ap.add_argument("-ms", "--messaging", default=0, type=int,
+	help="type of messaging (default is 0: REQ/REP; 1 is PUB/SUB, 2 is REQ/REP + PUB/SUB)")
+
 args = vars(ap.parse_args())
 
 # initialize the ImageHub object
-imageHub = imagezmq.ImageHub()
+if args["messaging"] == 0:
+	imageHub = imagezmq.ImageHub()
+elif args["messaging"] == 1:
+	imageHub = imagezmq.ImageHub(open_port='tcp://localhost:5566', REQ_REP = False)
+elif args["messaging"] == 2:
+	imageHub_rr = imagezmq.ImageHub()
+else:
+	raise ValueError("messaging input value must be 0, 1, or 2")
 
-# initialize the list of class labels MobileNet SSD was trained to
-# detect, then generate a set of bounding box colors for each class
-CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor"]
+# # initialize the list of class labels MobileNet SSD was trained to
+# # detect, then generate a set of bounding box colors for each class
+# CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+# 	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+# 	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+# 	"sofa", "train", "tvmonitor"]
 
-# load our serialized model from disk
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+# # load our serialized model from disk
+# print("[INFO] loading model...")
+# net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-# initialize the consider set (class labels we care about and want
-# to count), the object count dictionary, and the frame  dictionary
-CONSIDER = set(["dog", "person", "car"])
-objCount = {obj: 0 for obj in CONSIDER}
-frameDict = {}
+# # initialize the consider set (class labels we care about and want
+# # to count), the object count dictionary, and the frame  dictionary
+# CONSIDER = set(["dog", "person", "car"])
+# objCount = {obj: 0 for obj in CONSIDER}
+# frameDict = {}
 
 # initialize the dictionary which will contain  information regarding
 # when a device was last active, then store the last time the check
@@ -57,116 +68,139 @@ ESTIMATED_NUM_PIS = 4
 ACTIVE_CHECK_PERIOD = 10
 ACTIVE_CHECK_SECONDS = ESTIMATED_NUM_PIS * ACTIVE_CHECK_PERIOD
 
-# assign montage width and height so we can view all incoming frames
-# in a single "dashboard"
-mW = args["montageW"]
-mH = args["montageH"]
-print("[INFO] detecting: {}...".format(", ".join(obj for obj in
-	CONSIDER)))
+# # assign montage width and height so we can view all incoming frames
+# # in a single "dashboard"
+# mW = args["montageW"]
+# mH = args["montageH"]
+# print("[INFO] detecting: {}...".format(", ".join(obj for obj in
+# 	CONSIDER)))
 
+
+
+count=0
+fps = FPS().start()
+ 
 # start looping over all the frames
 while True:
-	# receive RPi name and frame from the RPi and acknowledge
+	# receive client name and frame from the client and acknowledge
 	# the receipt
-	(rpiName, frame) = imageHub.recv_image()
-	imageHub.send_reply(b'OK')
+	if args["messaging"] == 2 and count == 0:
+		(clientIP, frame) = imageHub_rr.recv_image()
+		imageHub_rr.send_reply(b'OK')
+		imageHub = imagezmq.ImageHub(open_port='tcp://{}:5566'.format(clientIP), REQ_REP = False)
 
-	# if a device is not in the last active dictionary then it means
-	# that its a newly connected device
-	if rpiName not in lastActive.keys():
-		print("[INFO] receiving data from {}...".format(rpiName))
+	else:
+		(clientIP, frame) = imageHub.recv_image()
+		if args["messaging"] == 0:
+			imageHub.send_reply(b'OK')
+	
 
-	# record the last active time for the device from which we just
-	# received a frame
-	lastActive[rpiName] = datetime.now()
 
-	# resize the frame to have a maximum width of 400 pixels, then
-	# grab the frame dimensions and construct a blob
-	frame = imutils.resize(frame, width=400)
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-		0.007843, (300, 300), 127.5)
+	if count<500:
+		count +=1 
+		fps.update()
+	if count==500:
+		fps.stop()
+		print("[INFO] elasped time to receive 500 frames: {:.2f}".format(fps.elapsed()))
+		print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+		count +=1
 
-	# pass the blob through the network and obtain the detections and
-	# predictions
-	net.setInput(blob)
-	detections = net.forward()
+	# # if a device is not in the last active dictionary then it means
+	# # that its a newly connected device
+	# if clientIP not in lastActive.keys():
+	# 	print("[INFO] receiving data from {}...".format(clientIP))
 
-	# reset the object count for each object in the CONSIDER set
-	objCount = {obj: 0 for obj in CONSIDER}
+	# # record the last active time for the device from which we just
+	# # received a frame
+	# lastActive[clientIP] = datetime.now()
 
-	# loop over the detections
-	for i in np.arange(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
-		# the prediction
-		confidence = detections[0, 0, i, 2]
+	# # resize the frame to have a maximum width of 400 pixels, then
+	# # grab the frame dimensions and construct a blob
+	# frame = imutils.resize(frame, width=400)
+	# (h, w) = frame.shape[:2]
+	# blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+	# 	0.007843, (300, 300), 127.5)
 
-		# filter out weak detections by ensuring the confidence is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
-			# extract the index of the class label from the
-			# detections
-			idx = int(detections[0, 0, i, 1])
+	# # # pass the blob through the network and obtain the detections and
+	# # # predictions
+	# # net.setInput(blob)
+	# # detections = net.forward()
 
-			# check to see if the predicted class is in the set of
-			# classes that need to be considered
-			if CLASSES[idx] in CONSIDER:
-				# increment the count of the particular object
-				# detected in the frame
-				objCount[CLASSES[idx]] += 1
+	# # # reset the object count for each object in the CONSIDER set
+	# # objCount = {obj: 0 for obj in CONSIDER}
 
-				# compute the (x, y)-coordinates of the bounding box
-				# for the object
-				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-				(startX, startY, endX, endY) = box.astype("int")
+	# # # loop over the detections
+	# # for i in np.arange(0, detections.shape[2]):
+	# # 	# extract the confidence (i.e., probability) associated with
+	# # 	# the prediction
+	# # 	confidence = detections[0, 0, i, 2]
 
-				# draw the bounding box around the detected object on
-				# the frame
-				cv2.rectangle(frame, (startX, startY), (endX, endY),
-					(255, 0, 0), 2)
+	# # 	# filter out weak detections by ensuring the confidence is
+	# # 	# greater than the minimum confidence
+	# # 	if confidence > args["confidence"]:
+	# # 		# extract the index of the class label from the
+	# # 		# detections
+	# # 		idx = int(detections[0, 0, i, 1])
 
-	# draw the sending device name on the frame
-	cv2.putText(frame, rpiName, (10, 25),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+	# # 		# check to see if the predicted class is in the set of
+	# # 		# classes that need to be considered
+	# # 		if CLASSES[idx] in CONSIDER:
+	# # 			# increment the count of the particular object
+	# # 			# detected in the frame
+	# # 			objCount[CLASSES[idx]] += 1
 
-	# draw the object count on the frame
-	label = ", ".join("{}: {}".format(obj, count) for (obj, count) in
-		objCount.items())
-	cv2.putText(frame, label, (10, h - 20),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255,0), 2)
+	# # 			# compute the (x, y)-coordinates of the bounding box
+	# # 			# for the object
+	# # 			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+	# # 			(startX, startY, endX, endY) = box.astype("int")
 
-	# update the new frame in the frame dictionary
-	frameDict[rpiName] = frame
+	# # 			# draw the bounding box around the detected object on
+	# # 			# the frame
+	# # 			cv2.rectangle(frame, (startX, startY), (endX, endY),
+	# # 				(255, 0, 0), 2)
 
-	# build a montage using images in the frame dictionary
-	montages = build_montages(frameDict.values(), (w, h), (mW, mH))
+	# # # draw the sending device name on the frame
+	# # cv2.putText(frame, clientIP, (10, 25),
+	# # 	cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-	# display the montage(s) on the screen
-	for (i, montage) in enumerate(montages):
-		cv2.imshow("Home pet location monitor ({})".format(i),
-			montage)
+	# # # draw the object count on the frame
+	# # label = ", ".join("{}: {}".format(obj, count) for (obj, count) in
+	# # 	objCount.items())
+	# # cv2.putText(frame, label, (10, h - 20),
+	# # 	cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255,0), 2)
 
-	# detect any kepresses
-	key = cv2.waitKey(1) & 0xFF
+	# # update the new frame in the frame dictionary
+	# frameDict[clientIP] = frame
 
-	# if current time *minus* last time when the active device check
-	# was made is greater than the threshold set then do a check
-	if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
-		# loop over all previously active devices
-		for (rpiName, ts) in list(lastActive.items()):
-			# remove the RPi from the last active and frame
-			# dictionaries if the device hasn't been active recently
-			if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
-				print("[INFO] lost connection to {}".format(rpiName))
-				lastActive.pop(rpiName)
-				frameDict.pop(rpiName)
+	# # build a montage using images in the frame dictionary
+	# montages = build_montages(frameDict.values(), (w, h), (mW, mH))
 
-		# set the last active check time as current time
-		lastActiveCheck = datetime.now()
+	# # display the montage(s) on the screen
+	# for (i, montage) in enumerate(montages):
+	# 	cv2.imshow("Video received from client ({})".format(i),
+	# 		montage)
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+	# # detect any kepresses
+	# key = cv2.waitKey(1) & 0xFF
+
+	# # if current time *minus* last time when the active device check
+	# # was made is greater than the threshold set then do a check
+	# if (datetime.now() - lastActiveCheck).seconds > ACTIVE_CHECK_SECONDS:
+	# 	# loop over all previously active devices
+	# 	for (clientIP, ts) in list(lastActive.items()):
+	# 		# remove the client from the last active and frame
+	# 		# dictionaries if the device hasn't been active recently
+	# 		if (datetime.now() - ts).seconds > ACTIVE_CHECK_SECONDS:
+	# 			print("[INFO] lost connection to {}".format(clientIP))
+	# 			lastActive.pop(clientIP)
+	# 			frameDict.pop(clientIP)
+
+	# 	# set the last active check time as current time
+	# 	lastActiveCheck = datetime.now()
+
+	# # if the `q` key was pressed, break from the loop
+	# if key == ord("q"):
+	# 	break
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
